ac60590dd1804b522ffdaeede1525c503d4ceee3
zhennann <zhen.nann@icloud.com>
2023-12-15 09:32:33 +0800

    enhance: same mode

--- src/main.js
+++ src/main.js
@@ -1,28 +1,37 @@
 const is = require('is-type-of');
 
+const __internalPropsStatic = ['length', 'name', 'prototype'];
+const __internalPropsPrototype = ['constructor'];
+
 module.exports = function mixinClasses(classMain, classesMore, ...args) {
   // classesMore
   if (!Array.isArray(classesMore)) {
     classesMore = [classesMore];
   }
   // classMain
+  let isFunction;
   if (is.function(classMain) && !is.class(classMain)) {
     classMain = classMain(...args);
+    isFunction = true;
+  } else {
+    isFunction = false;
   }
   if (classMain.__eb_mixined) return classMain;
   // classesMore
   const mixins = [];
   for (const _class of classesMore) {
     if (is.function(_class) && !is.class(_class)) {
+      if (!isFunction) throw new Error('should use the same mixin mode as the main class');
       mixins.push(_class(...args));
     } else {
+      if (isFunction) throw new Error('should use the same mixin mode as the main class');
       mixins.push(_class);
     }
   }
   // mixin
   for (const mixin of mixins) {
-    copyProperties(classMain, mixin); // copy static
-    copyProperties(classMain.prototype, mixin.prototype); // copy prototype
+    copyProperties(classMain, mixin, __internalPropsStatic); // copy static
+    copyProperties(classMain.prototype, mixin.prototype, __internalPropsPrototype); // copy prototype
   }
   // record
   classMain.__eb_mixined = true;
@@ -30,9 +39,9 @@ module.exports = function mixinClasses(classMain, classesMore, ...args) {
   return classMain;
 };
 
-function copyProperties(target, source) {
+function copyProperties(target, source, __internalProps) {
   for (const key of Reflect.ownKeys(source)) {
-    if (key !== 'constructor' && key !== 'prototype' && key !== 'name') {
+    if (!__internalProps.includes(key)) {
       const desc = Object.getOwnPropertyDescriptor(source, key);
       Object.defineProperty(target, key, desc);
     }
